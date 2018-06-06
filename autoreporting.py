b5127e0fbb6cf87df92883d2f6ae46f95e8a8ca0
Goyder <joshua@goyder.net.au>
2018-06-06 22:34:58 +0800

    :sparkles: Add functionality to read and publish tables from .csv file.

--- autoreporting.py
+++ autoreporting.py
@@ -1,4 +1,20 @@
+import pandas as pd
 from jinja2 import FileSystemLoader, Environment
+
+# Allow for very wide columns - otherwise columns are spaced and ellipse'd
+pd.set_option("display.max_colwidth", 200)
+
+
+def csv_to_html(filepath):
+    """
+    Open a .csv file and return it in HTML format.
+    :param filepath: Filepath to a .csv file to be read.
+    :return: String of HTML to be published.
+    """
+    df = pd.read_csv(filepath, index_col=0)
+    html = df.to_html()
+    return html
+
 
 # Configure Jinja and ready the loader
 env = Environment(
@@ -15,12 +31,12 @@ sections = list()
 sections.append(table_section_template.render(
     model="VGG19",
     dataset="VGG19_results.csv",
-    table="Table goes here."
+    table=csv_to_html("datasets/VGG19_results.csv")
 ))
 sections.append(table_section_template.render(
     model="MobileNet",
     dataset="MobileNet_results.csv",
-    table="Table goes here."
+    table=csv_to_html("datasets/MobileNet_results.csv")
 ))
 
 
